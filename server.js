54eb298f5f879fc8ff4093921426638a0c885a6b
Jason Alvarado <alvaradojasondev@gmail.com>
2020-08-21 19:17:34 -0300

    feat: clean code

--- server.js
+++ server.js
@@ -3,15 +3,32 @@ const ytdl = require("ytdl-core");
 
 const bot = new Discord.Client();
 
-const token = "NzQ2NDI3OTcwNzU4OTAxODMz.X0ALOA.PaHUP49qwjKHuOMg9ryuS32xfr0";
 
 const prefixImage = "./images/";
 const prefixCommand = "!";
 
 const siuAudio = "https://www.youtube.com/watch?v=CGDYmD-ZyoY";
+const primoCocoSily = "https://www.youtube.com/watch?v=zNgOKYx_khk";
 
 function login() {
   bot.login(token);
+}
+
+function PlayAudio(url) {
+  const voiceChannel = msg.member.voice.channel;
+
+  if (!voiceChannel) {
+    return msg.reply("entra a un canal mongoloide");
+  }
+
+  voiceChannel.join().then((connection) => {
+    const stream = ytdl(url, {
+      filter: "audioonly",
+    });
+    const dispatcher = connection.play(stream);
+
+    dispatcher.on("finish", () => voiceChannel.leave());
+  });
 }
 
 bot.on("ready", () => {
@@ -20,20 +37,11 @@ bot.on("ready", () => {
 
 bot.on("message", (msg) => {
   if (msg.content === prefixCommand + "si") {
-    const voiceChannel = msg.member.voice.channel;
+    PlayAudio(siuAudio);
+  }
 
-    if (!voiceChannel) {
-      return msg.reply("entra a un canal mongoloide");
-    }
-
-    voiceChannel.join().then((connection) => {
-      const stream = ytdl(siuAudio, {
-        filter: "audioonly",
-      });
-      const dispatcher = connection.play(stream);
-
-      dispatcher.on("finish", () => voiceChannel.leave());
-    });
+  if (msg.content === prefixCommand + "iii") {
+    PlayAudio(siuAudio);
   }
 });
 
@@ -43,41 +51,31 @@ bot.on("message", (msg) => {
       files: [prefixImage + "teLaCreisteWe.jpg"],
     });
   }
-});
 
-bot.on("message", (msg) => {
   if (msg.content === prefixCommand + "ward") {
     msg.channel.send({
       files: [prefixImage + "ward.jpg"],
     });
   }
-});
 
-bot.on("message", (msg) => {
   if (msg.content === prefixCommand + "xd") {
     msg.channel.send({
       files: [prefixImage + "xd.jpg"],
     });
   }
-});
 
-bot.on("message", (msg) => {
   if (msg.content === prefixCommand + "name") {
     msg.channel.send({
       files: [prefixImage + "name.jpg"],
     });
   }
-});
 
-bot.on("message", (msg) => {
   if (msg.content === ":virgo_senial:") {
     msg.channel.send({
       files: [prefixImage + "ranked.jpg"],
     });
   }
-});
 
-bot.on("message", (msg) => {
   if (msg.content === prefixCommand + "alv") {
     msg.channel.send({
       files: [prefixImage + "alv.jpg"],
